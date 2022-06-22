1d130e6d528c217a9b05e41942cd49ce58f943e4
tonia <tonia@cia.como>
2022-06-22 14:27:02 +0200

    aggiunta classe prodotto

--- src/jana60shop/Prodotto.java
+++ src/jana60shop/Prodotto.java
@@ -1,5 +1,83 @@
 package jana60shop;
 
-public class Prodotto {
+import java.text.DecimalFormat;
 
+public class Prodotto {
+	DecimalFormat df = new DecimalFormat();
+	
+/*Creare la classe Prodotto che gestisce i prodotti dello shop. Un prodotto è caratterizzato da:
+- codice (numero intero)
+- nome
+- marca
+- prezzo
+- iva
+Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri 
+metodi di “utilità” per fare in modo che:
+- il codice prodotto sia accessibile solo in lettura
+- gli altri attributi siano accessibili sia in lettura che in scrittura
+- il prodotto esponga un metodo per avere il prezzo comprensivo di iva formattato*/
+	
+	private int codice;
+	private String nome;
+	private String marca;
+	private double prezzo;
+	private int iva;
+	
+	//costruttore
+	public Prodotto(int codice, String nome, String marca, double prezzo, int iva) {
+		super();
+		this.codice = codice;
+		this.nome = nome;
+		this.marca = marca;
+		this.prezzo = prezzo;
+		this.iva = iva;
+	}
+	
+	//getter e setter
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public int getIva() {
+		return iva;
+	}
+
+	public void setIva(int iva) {
+		this.iva = iva;
+	}
+
+	public int getCodice() {
+		return codice;
+	}
+	
+	//metodo iva
+	public double calcoloPrezzoIva(double prezzo, int iva) {
+		double prezzoFinale = prezzo * iva % 100;
+		System.out.println("Il prezzo del prodotto scelto è: " + df.format(prezzoFinale));
+	return prezzoFinale;
+		
+	}
+	
+	
+	
 }
